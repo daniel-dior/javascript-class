7ac28053960f6eaec9191699c265b2344449c8d3
daniel-logo <daniel-logo>
2024-03-21 16:34:58 +0100

    commit

--- assignment 06/main.js
+++ assignment 06/main.js
@@ -1,49 +1,30 @@
+const tableData= document.getElementById('tableData')
 const logMovies = async () => {
-    // let loading = true;
     const response = await fetch("https://jsonplaceholder.org/users");
-    // loading = false;
-    console.log(response);
-
-    setTimeout(() => {
-        if (!response) {
-          document.getElementById('result').innerHTML = 'Loading...'; 
-        }
-      }, 1000)
     const movies = await response.json();
     console.log(movies);
-    const table = document.getElementsByTagName("table")[0];
-    for (i = 0; i < movies.length; i++) {
-        document.getElementById("demo").innerHTML = movies.map(getFullname);
-        document.getElementById("demo2").innerHTML = movies.map(getFullemail);
-        document.getElementById("demo3").innerHTML = movies.map(getFulladdress);
-        document.getElementById("demo4").innerHTML = movies.map(getFullid);
-        document.getElementById("demo5").innerHTML = movies.map(getFullphone);
-        document.getElementById("demo6").innerHTML = movies.map(getFullwebsite);
-        // const myArray = text.split(" ");
-
-  }
-};
-
-document.getElementById("clicky").addEventListener("click", () => {
-    logMovies()
-
-}
-);
-function getFullname(item) {
-  return [ `<li>${item.firstname}, ${item.lastname}</li> `]
-}
-function getFullemail(item) {
-  return [ ` <li>${item.email} </li> `]
-}
-function getFulladdress(item) {
-  return [ `<li>${item.address.street}</li> `]
-}
-function getFullid(item) {
-  return [ `<li>${item.id}</li> `]
-}
-function getFullphone(item) {
-  return [ `<li>${item.phone}</li> `]
-}
-function getFullwebsite(item) {
-  return [ `<li>${item.website}</li> `]
-}
+    let i = 0;
+    for (i = 0; i < movies.length; i++){
+        const tableRow = document.createElement('tr');
+        const item = movies[i];
+        const childData =`<tr><td>${item.firstname}, ${item.lastname}</td>
+  <td>${item.phone}</td>
+  <td>${item.email}</td>
+  <td>${item.address.street}</td>
+  <td>${item.birthDate}</td>
+  <td>${item.company.name}</td></tr>`;
+  tableRow.innerHTML = childData
+  tableData.appendChild(tableRow)
+    }
+  };
+  document.getElementById("clicky").addEventListener("click", () => {
+    logMovies();
+  });
+  // async function myDisplay() {
+  //   let myPromise = new Promise(function(resolve) {
+  //     setTimeout(function() {resolve("loading....");}, 9000);
+  //   });
+  //   document.getElementById("demo").innerHTML = await myPromise;
+  //   myDisplay();
+  // }
+  

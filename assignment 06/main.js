fd4e1ae30e0a8604ff81529a4e55cd0f5c2b372e
daniel-logo <daniel-logo>
2024-03-26 14:55:07 +0100

    loading fuction

--- assignment 06/main.js
+++ assignment 06/main.js
@@ -1,23 +1,27 @@
+let load = document.getElementById("load")
+load.innerHTML = `<h1>loading...</h1>`;
 const tableData= document.getElementById('tableData')
 const logMovies = async () => {
-    const response = await fetch("https://jsonplaceholder.org/users");
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const movies = await response.json();
     console.log(movies);
+    load.style.display = 'none'
     let i = 0;
     for (i = 0; i < movies.length; i++){
         const tableRow = document.createElement('tr');
         const item = movies[i];
-        const childData =`<tr><td>${item.firstname}, ${item.lastname}</td>
+        const childData =`<tr><td>${item.name}</td>
   <td>${item.phone}</td>
   <td>${item.email}</td>
   <td>${item.address.street}</td>
-  <td>${item.birthDate}</td>
+  <td>${item.website}</td>
   <td>${item.company.name}</td></tr>`;
   tableRow.innerHTML = childData
   tableData.appendChild(tableRow)
     }
   };
   document.getElementById("clicky").addEventListener("click", () => {
+    load.style.display = 'block'
     logMovies();
   });
   // async function myDisplay() {
